004b5d56f32ce6d4ffdd4ae41990522874019276
Lenin <leninsavarimuthu8@gmail.com>
2025-09-20 16:52:40 +0530

    Day 1- initial commit

---
+++ type_casting.py
@@ -0,0 +1,15 @@
+# Type casting - The process of converting a variable from one data type to another
+
+name = "Lenin"
+age = 28
+gpa = 7.5
+is_student = True
+
+gpa = int(gpa)
+print(gpa)
+
+age = float(age)
+print(age)
+
+name = bool(name)
+print(name)
